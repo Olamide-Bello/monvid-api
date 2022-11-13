4b5e49255cb7d22470e15f70d41805bbdcca261a
Olamide-Bello <belloolamideakinniyi@gmail.com>
2022-11-13 23:46:50 +0100

    make it better

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 // const cartRouter = require('./routers/cart')
 require('./db/mongoose')
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 const app = express()
 
