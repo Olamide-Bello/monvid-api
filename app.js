c4453742db7f17af012381b6c699a30d98fb93c3
Olamide-Bello <belloolamideakinniyi@gmail.com>
2022-11-14 15:57:52 +0100

    configuration commit

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express')
-// const userRouter = require('./routers/user')
-// const itemRouter = require('./routers/item')
-// const cartRouter = require('./routers/cart')
+const userRouter = require('./routers/user')
+const itemRouter = require('./routers/item')
+const cartRouter = require('./routers/cart')
 require('./db/mongoose')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.json())
-// app.use(userRouter)
-// app.use(itemRouter)
-// app.use(cartRouter)
+app.use(userRouter)
+app.use(itemRouter)
+app.use(cartRouter)
 
 
+app.get('/', function(req, res, next) {
+    res.send("Hello world!");
+});
 app.listen(port, () => {
     console.log('server listening on port ' + port)
 })
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,6 @@
+require('dotenv').config ();
+const source = process.env.MONGODB_URL;
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(source, {
 useNewUrlParser: true,
 })
